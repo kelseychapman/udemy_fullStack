bb8825c68ff7ed6724103ad0c34706401b424dfd
kelseychapman <kelseyc.chapman@gmail.com>
2016-12-05 12:44:51 -0700

    basic js math

---
+++ unit8_introToJavascript/basic prompts/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+alert("Nice to meet you, " + firstName + " " + lastName + " " + age);
+console.log("Also great to meet you, " + firstName + " " + lastName + " " + age);
